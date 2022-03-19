5eec43fbbdd3eac66b761fa364aae23898e57219
Rian Kurniawan <riankurcyberdeepweb@gmail.com>
2022-03-19 13:33:30 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const client_id= process.env.REACT_APP_SPOTIFY_ID;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
